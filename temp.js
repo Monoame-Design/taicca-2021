fece1cccd1fdc337b6d201815ccf9cbff21d1176
ankycheng <ankycheng@gmail.com>
2021-07-04 22:43:20 +0800

    update code (remove js animation and vh in css)

--- temp.js
+++
@@ -1,7 +0,0 @@
-$.get('https://raw.githubusercontent.com/davidjbradshaw/iframe-resizer/master/js/iframeResizer.contentWindow.min.js', function (data) {
-    console.log(data)
-    $("head").append('<script>' + data + '<\/script>');
-    //   iFrameResize({
-    //   log: true
-    // });
-});
